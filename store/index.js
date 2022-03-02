b285ccb89c32b64abc35294cbbe7e4245b89c6c1
TakKokubu <kokubu.tk@gmail.com>
2022-03-02 14:24:37 +0900

    setup

---
+++ store/index.js
@@ -0,0 +1,18 @@
+import Vuex from 'vuex'
+
+const url = "http://localhost:5000/api"
+
+const createStore = () => {
+  return new Vuex.Store({
+    state: {
+    },
+    mutations: {
+    },
+    actions: {
+    },
+    getters: {
+    }
+  })
+}
+
+export default createStore
