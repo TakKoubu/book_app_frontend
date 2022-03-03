5eacf97e5ebd74bd387e0142fa50df856070a31b
TakKokubu <kokubu.tk@gmail.com>
2022-03-03 10:41:15 +0900

    book-index

---
+++ pages/books/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <BookPreview v-for="book in loadedBooks" :key="book.id" :book="book" />
+    {{ loadedBooks }}
+  </div>
+</template>
+
+<script>
+import BookPreview from "@/components/BookPreview";
+import { mapGetters } from "vuex";
+import { mapActions } from "vuex";
+
+export default {
+  components: {
+    BookPreview,
+  },
+  created() {
+    this.fetchBooks;
+  },
+  methods: {
+    ...mapActions(["fetchBooks"]),
+  },
+  computed: {
+    ...mapGetters(["loadedBooks"]),
+  },
+};
+</script>
--- store/index.js
+++ store/index.js
@@ -5,12 +5,27 @@ const url = "http://localhost:5000/api"
 const createStore = () => {
   return new Vuex.Store({
     state: {
+      loadedBooks: []
     },
     mutations: {
+      setBooks(state, books){
+        state.loadedBooks = books
+      }
     },
     actions: {
+      fetchBooks({ commit }){
+        return this.$axios
+        .get(`${url}/books`)
+        .then((res) => {
+          commit('setBooks', res.data)
+        })
+        .catch((e) => console.log(e))
+      }
     },
     getters: {
+      loadedBooks(state){
+        return state.loadedBooks;
+      }
     }
   })
 }
